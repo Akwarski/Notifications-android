278430613de6bbf20b3d069e0c5e99d96d20ba3d
Akwarski <akwarski@gmail.com>
2019-07-05 14:26:52 +0200

    add notification FCM v1

--- app/src/main/java/com/example/rxkotlin/MainActivity.kt
+++ app/src/main/java/com/example/rxkotlin/MainActivity.kt
@@ -4,19 +4,14 @@ import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.ImageSpan
 import android.util.Log
 import android.util.Patterns
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.core.content.ContextCompat
-import androidx.core.view.isEmpty
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
@@ -30,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var button : Button
     lateinit var warring: Drawable
+    var TAG = "main_activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,6 +65,31 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(motionLayout, "Successful", Snackbar.LENGTH_LONG).show()
         }
 
+        token()
+    }
+
+    private fun token(){
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d("foool", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    fun runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+        // [END fcm_runtime_enable_auto_init]
     }
 
     private fun emailField(){
@@ -123,6 +144,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    /*private fun notify(): Observable<Notification>{
+        *//*return Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return FirebaseInstanceId.getInstance().getToken();
+            }
+        });*//*
+
+
+        Observable.fromCallable()
+            .subscribe{
+
+            }
+    }*/
+
+    /*private fun notifySecond(): io.reactivex.Notification<Any>{
+
+    }*/
 
     private val moreThanSix = ObservableTransformer<String,String> { observable->
         observable.flatMap {
